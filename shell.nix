02784dd88ae1777792adf17636001c26ed947210
bcb <benjamincb@hotmail.dk>
2024-04-29 13:34:24 +0200

    add nix-shell

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    packages = with pkgs; [
+        nodejs
+        nodePackages.typescript
+        nodePackages.typescript-language-server
+        nodePackages.yaml-language-server
+    ];
+}
